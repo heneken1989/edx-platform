3e0b0ffdd36b77064c5eb34e5ef3a5728c09b245
heneken1989 <huyhien1989gmail.com>
2025-07-29 21:34:53 +0700

    aa

--- lms/djangoapps/payment/views.py
+++ lms/djangoapps/payment/views.py
@@ -27,8 +27,8 @@ def build_payment_url(path, **params):
         str: Complete URL
     """
     # Remove trailing slash from base_url if exists
-    #base_url = "https://nohongodrill.com"
-    base_url = "http://apps.local.openedx.io:2000"
+    base_url = "https://nohongodrill.com/learning/"
+    #base_url = "http://apps.local.openedx.io:2000"
     # Add path directly without /learning prefix
     url = f"{base_url}/{path}"
     
